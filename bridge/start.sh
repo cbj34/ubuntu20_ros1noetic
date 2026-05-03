7bd22c1e8405f9a358ebb4b9f1c2f328d17fcd4d
cbj34 <59981537+cbj34@users.noreply.github.com>
2026-05-03 17:04:38 +0200

    Update start.sh changed ros ip

--- bridge/start.sh
+++ bridge/start.sh
@@ -5,7 +5,7 @@ source /opt/ros/noetic/setup.bash
 
 # Use the environment variables provided, or default to localhost if not set
 export ROS_MASTER_URI=${ROS_MASTER_URI:-http://192.168.1.123:11311}
-export ROS_IP=${ROS_IP:-192.168.2.211}
+export ROS_IP=${ROS_IP:-192.168.1.211}
 
 echo "ROS_MASTER_URI is set to: $ROS_MASTER_URI"
 echo "ROS_IP is set to: $ROS_IP"
